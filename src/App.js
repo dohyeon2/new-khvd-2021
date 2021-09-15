cc6c40c0a8b42a644f356327e7b2768d88fd2ba8
Ubuntu <ubuntu@ip-172-31-61-96.ap-northeast-2.compute.internal>
2021-09-15 07:23:43 +0000

    [feat]react-router-dom 적용

--- src/App.js
+++ src/App.js
@@ -5,38 +5,48 @@ import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { setUser, setUserSuccess } from './modules/user';
 import { googleAPIClientID } from './vars/credential';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const EditorContainer = styled.div`
     border:1px solid #000;
 `;
 
 function App() {
+  //global states
   const { user } = useSelector(s => s);
   const dispatch = useDispatch();
+
+  //구글 로그인 응답
   const responseGoogle = (response) => {
     dispatch(setUser());
     dispatch(setUserSuccess(response));
   }
+
   useEffect(() => {
     console.log(user);
   }, [user, googleAPIClientID]);
 
-  
   return (
-    <div className="App">
-      <GoogleLogin
-        clientId={googleAPIClientID}
-        buttonText="Login"
-        onSuccess={responseGoogle}
-        onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}
-        isSignedIn={true}
-      />
-      <GoogleLogout />
-      <EditorContainer>
-        <Editor />
-      </EditorContainer>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <div className="app">
+            <GoogleLogin
+              clientId={googleAPIClientID}
+              buttonText="Login"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={'single_host_origin'}
+              isSignedIn={true}
+            />
+            <GoogleLogout />
+            <EditorContainer>
+              <Editor />
+            </EditorContainer>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
